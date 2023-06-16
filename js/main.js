ed1b2096033ca743e89444e5af0186b8d2f375b9
Nicolas <nicolas_jofre@outlook.com.ar>
2023-06-15 21:22:24 -0300

    Arreglo en nav bar con media

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 //Inicializador de AOS animaciones (armadas)
 AOS.init();
 
-let menu = document.querySelector("menu");
-let menubar = document.querySelector("menubar");
+let menu = document.querySelector("#menu");
+let menu_bar = document.querySelector("#menu-bar");
 
+//Evento click de boton hambuerguer
+
+menu_bar.addEventListener('click', () => {
+   menu.classList.toggle("menu-toggle"); 
+})
 
